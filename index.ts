6b1f78f745c1abefc92ddccdfd5359d6191274a3
fahadimaduddin <fahaduddin1997@gmail.com>
2024-04-03 14:03:01 +0500

    tweak the text

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-  { message: "Enter first number:", type: "number", name: "firstNumber" },
-  { message: "Enter second number:", type: "number", name: "secondNumber" },
+  { message: "Enter your first number:", type: "number", name: "firstNumber" },
+  { message: "Enter your second number:", type: "number", name: "secondNumber" },
   {
     message: "Select one the operators to perform operation",
     type: "list",
